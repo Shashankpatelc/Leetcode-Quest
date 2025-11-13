0665cbeb6374b236a7a145ff46dfc0dad627fc12
Shashankpatelc <s0445443@gmail.com>
2025-11-13 15:34:41 +0530

    Leetcode Solution

---
+++ Q1._Concatenation_of_Array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n*2, 0);
+        for(int i = 0; i < n; i++ ){
+            ans[i] = nums[i];
+            ans[n + i] = nums[i];
+        }
+        return ans;
+    }
+};
